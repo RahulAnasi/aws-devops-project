fc9d81b341c25eb57bcfa152824d7b0d114751fd
rahulanasibsc@gmail.com <rahul@RahulAnasi.localdomain>
2026-04-16 09:43:26 +0000

    add redis caching

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const http = require('http');
 const mysql = require('mysql2');
+const redis = require('redis');
 
 console.log("Starting app...");
 
-// DB connection
+// Redis client
+const redisClient = redis.createClient({
+  socket: {
+    host: 'master.aws-devops-cache-cluster.6nzr6z.eun1.cache.amazonaws.com:6379',
+    port: 6379
+  }
+});
+
+redisClient.connect().then(() => {
+  console.log("✅ Connected to Redis");
+}).catch(err => {
+  console.error("❌ Redis connection failed:", err);
+});
+
+// MySQL connection
 const connection = mysql.createConnection({
   host: 'aws-devops-db.c5oqk08mi0us.eu-north-1.rds.amazonaws.com',
   user: 'admin',
-  password: 'Admin10906'
+  password: 'Devops123'
 });
 
-// Try connecting
 connection.connect((err) => {
   if (err) {
     console.error('❌ DB connection failed:', err);
@@ -18,42 +32,56 @@ connection.connect((err) => {
   }
   console.log('✅ Connected to DB');
 
-  // Create DB
-  connection.query("CREATE DATABASE IF NOT EXISTS devopsdb", (err) => {
-    if (err) {
-      console.error("DB create error:", err);
-      return;
-    }
+  connection.query("CREATE DATABASE IF NOT EXISTS devopsdb");
+  connection.query("USE devopsdb");
 
-    connection.query("USE aws-devops-db");
-
-    connection.query(`
-      CREATE TABLE IF NOT EXISTS messages (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        text VARCHAR(255)
-      )
-    `);
-  });
+  connection.query(`
+    CREATE TABLE IF NOT EXISTS messages (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      text VARCHAR(255)
+    )
+  `);
 });
 
 // Server
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
-  connection.query(
-    "INSERT INTO messages (text) VALUES ('Hello from AWS 🚀')"
-  );
+  try {
+    // 1. Check cache
+    const cached = await redisClient.get("messages");
 
-  connection.query(
-    "SELECT * FROM messages",
-    (err, results) => {
-      if (err) {
-        res.end('Error fetching data');
-        return;
-      }
-
-      res.end(JSON.stringify(results));
+    if (cached) {
+      console.log("⚡ Serving from Redis cache");
+      res.end(cached);
+      return;
     }
-  );
+
+    console.log("📦 Fetching from DB");
+
+    // 2. Fetch from DB
+    connection.query(
+      "SELECT * FROM messages",
+      async (err, results) => {
+        if (err) {
+          res.end("DB error");
+          return;
+        }
+
+        const data = JSON.stringify(results);
+
+        // 3. Store in Redis
+        await redisClient.set("messages", data, {
+          EX: 10 // expires in 10 seconds
+        });
+
+        res.end(data);
+      }
+    );
+
+  } catch (error) {
+    console.error(error);
+    res.end("Error");
+  }
 });
 
 server.listen(3000, () => {
